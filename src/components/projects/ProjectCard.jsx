7201f07d565cd668910860f2d999dcda22029050
Piyush <91675982+PiyushKBhattacharyya@users.noreply.github.com>
2025-01-29 02:24:56 +0530

    adding style in project card

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,90 +1,74 @@
 import React, { useContext } from 'react';
-import {
-  Button, Card, Badge, Col,
-} from 'react-bootstrap';
+import { Button, Card, Badge, Col } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { ThemeContext } from 'styled-components';
 import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
 
-const styles = {
-  badgeStyle: {
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
-    margin: 5,
-  },
-  cardStyle: {
-    borderRadius: 10,
-  },
-  cardTitleStyle: {
-    fontSize: 24,
-    fontWeight: 700,
-  },
-  cardTextStyle: {
-    textAlign: 'left',
-  },
-  linkStyle: {
-    textDecoration: 'none',
-    padding: 10,
-  },
-  buttonStyle: {
-    margin: 5,
-  },
-};
+const BadgeStyled = styled(Badge)`
+  padding: 5px 10px;
+  margin: 5px;
+`;
 
-const ProjectCard = (props) => {
+const CardStyled = styled(Card)`
+  border-radius: 10px;
+  background-color: ${(props) => props.theme.cardBackground};
+  border-color: ${(props) => props.theme.cardBorderColor};
+`;
+
+const CardTitleStyled = styled(Card.Title)`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+const CardTextStyled = styled(Card.Text)`
+  text-align: left;
+`;
+
+const ButtonStyled = styled(Button)`
+  margin: 5px;
+`;
+
+const ProjectCard = ({ project }) => {
   const theme = useContext(ThemeContext);
+
   const parseBodyText = (text) => <ReactMarkdown children={text} />;
 
-  const { project } = props;
+  if (!project) return null;
+
+  const { title, bodyText, image, links, tags } = project;
 
   return (
     <Col>
-      <Card
-        style={{
-          ...styles.cardStyle,
-          backgroundColor: theme.cardBackground,
-          borderColor: theme.cardBorderColor,
-        }}
-        text={theme.bsSecondaryVariant}
-      >
-        <Card.Img variant="top" src={project?.image} />
+      <CardStyled text={theme.bsSecondaryVariant}>
+        {image && <Card.Img variant="top" src={image} />}
         <Card.Body>
-          <Card.Title style={styles.cardTitleStyle}>{project.title}</Card.Title>
-          <Card.Text style={styles.cardTextStyle}>
-            {parseBodyText(project.bodyText)}
-          </Card.Text>
+          <CardTitleStyled>{title}</CardTitleStyled>
+          <CardTextStyled>{parseBodyText(bodyText)}</CardTextStyled>
         </Card.Body>
 
         <Card.Body>
-          {project?.links?.map((link) => (
-            <Button
+          {links?.map((link) => (
+            <ButtonStyled
               key={link.href}
-              style={styles.buttonStyle}
               variant={'outline-' + theme.bsSecondaryVariant}
               onClick={() => window.open(link.href, '_blank')}
             >
               {link.text}
-            </Button>
+            </ButtonStyled>
           ))}
         </Card.Body>
-        {project.tags && (
+
+        {tags && (
           <Card.Footer style={{ backgroundColor: theme.cardFooterBackground }}>
-            {project.tags.map((tag) => (
-              <Badge
-                key={tag}
-                pill
-                bg={theme.bsSecondaryVariant}
-                text={theme.bsPrimaryVariant}
-                style={styles.badgeStyle}
-              >
+            {tags.map((tag) => (
+              <BadgeStyled key={tag} pill bg={theme.bsSecondaryVariant} text={theme.bsPrimaryVariant}>
                 {tag}
-              </Badge>
+              </BadgeStyled>
             ))}
           </Card.Footer>
         )}
-      </Card>
+      </CardStyled>
     </Col>
   );
 };
@@ -94,10 +78,12 @@ ProjectCard.propTypes = {
     title: PropTypes.string.isRequired,
     bodyText: PropTypes.string.isRequired,
     image: PropTypes.string,
-    links: PropTypes.arrayOf(PropTypes.shape({
-      text: PropTypes.string.isRequired,
-      href: PropTypes.string.isRequired,
-    })),
+    links: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        href: PropTypes.string.isRequired,
+      })
+    ),
     tags: PropTypes.arrayOf(PropTypes.string),
   }).isRequired,
 };
